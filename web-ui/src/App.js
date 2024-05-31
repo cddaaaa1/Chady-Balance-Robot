f44458bad15cd27cbee9b130da4cb1af47e8b38e
Tingxu Chen <115492152+ccchloe510@users.noreply.github.com>
2024-05-31 13:42:12 +0100

    without ui

--- web-ui/src/App.js
+++ web-ui/src/App.js
@@ -1,33 +1,30 @@
 import React from 'react';
 import axios from 'axios';
-import './App.css';  
 
-function App() {
-  const sendCommand = (command) => {
-    axios.post('http://172.20.10.6:5001/move', { command }) // IP address of the server
-      .then(response => {
-        console.log(response.data);
-      })
-      .catch(error => {
-        console.error("There was an error sending the command!", error);
-      });
-  };
+ function App() {
+   const sendCommand = (command) => {
+     axios.post('http://172.20.10.6:5001/move', { command }) // IP address of the server
+       .then(response => {
+         console.log(response.data);
+       })
+       .catch(error => {
+         console.error("There was an error sending the command!", error);
+       });
+   };
 
-  return (
-    <div className="App">
-      <h1>Robot Control</h1>
-      <div className="button-container">
-        <button className="control-button button1" onClick={() => sendCommand('switch_to_auto')}>Automatic Mode</button>
-        <button className="control-button button2" onClick={() => sendCommand('switch_to_manual')}>Manual Mode</button>
-        <button className="control-button button3" onClick={() => sendCommand('forward')}>Move Forward</button>
-        <button className="control-button button4" onClick={() => sendCommand('backward')}>Move Backward</button>
-        <button className="control-button button5" onClick={() => sendCommand('left')}>Move Left</button>
-        <button className="control-button button6" onClick={() => sendCommand('right')}>Move Right</button>
-        <button className="control-button button7" onClick={() => sendCommand('stop')}>Stop</button>
-      </div>
-    </div>
-  );
-}
+   return (
+     <div>
+       <h1>Robot Control</h1>
+       <button onClick={() => sendCommand('switch_to_auto')}>Automatic Mode</button>
+       <button onClick={() => sendCommand('switch_to_manual')}>Manual Mode</button>
+       <button onClick={() => sendCommand('forward')}>Move Forward</button>
+       <button onClick={() => sendCommand('backward')}>Move Backward</button>
+       <button onClick={() => sendCommand('left')}>Move Left</button>
+       <button onClick={() => sendCommand('right')}>Move Right</button>
+       <button onClick={() => sendCommand('stop')}>Stop</button>
+     </div>
+   );
+ }
 
-export default App;
+ export default App;
 
